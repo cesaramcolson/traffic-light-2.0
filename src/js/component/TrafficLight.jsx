aa11a9503e8dbeea6e5c0b8b355d81903c37f4e4
Cesar Amaya Colella <cesar.rafael.gep@gmail.com>
2025-01-16 13:06:29 +0000

    glows when clicked, using state to set the glow onClick

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 
 const TrafficLight = () => {
+	const [color, setColor] = useState("red")
+
 	return (
 		<div className="container">
 			<div className="pole"></div>
 			<div className="structure">
-				<div className="light red"></div>
-				<div className="light yellow"></div>
-				<div className="light green"></div>
+				<div onClick={() => setColor("red")} className={"light red" + (color === "red" ? " glow-red" : "")}></div>
+				<div onClick={() => setColor("yellow")} className={"light yellow" + (color === "yellow" ? " glow-yellow" : "")}></div>
+				<div onClick={() => setColor("green")} className={"light green" + (color === "green" ? " glow-green" : "")}></div>
 			</div>
 		</div>
 	);
