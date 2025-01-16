3dcbce31bcb8b35a0894c5272773789434e26f50
Cesar Amaya Colella <cesar.rafael.gep@gmail.com>
2025-01-16 13:38:22 +0000

    purple color added and glowing when clicked

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -1,19 +1,26 @@
 import React, {useEffect, useState} from "react";
 
-const TrafficLight = () => {
-	const [color, setColor] = useState("red")
+const initialColors = ["red", "yellow", "green"];
 
-	const colors = ["red", "yellow", "green"];
+const TrafficLight = () => {
+	const [colors, setColors] = useState(initialColors);
+	const [color, setColor] = useState("red")
 
 	useEffect(() => {
 		const interval = setInterval(() => {
-			const currentColor = colors.indexOf(color);
-			const nextColor = (currentColor + 1) % colors.length;
-			setColor(colors[nextColor])
+			const currentColor = initialColors.indexOf(color);
+			const nextColor = (currentColor + 1) % initialColors.length;
+			setColor(initialColors[nextColor])
 		}, 3000);
 
 		return () => clearInterval(interval);
-	}, [color])
+	}, [color, colors])
+
+	const addPurple = () => {
+		if (!colors.includes("purple")) {
+			setColors ([...colors, "purple"]);
+		}
+	};
 
 	return (
 		<div className="container">
@@ -22,9 +29,12 @@ const TrafficLight = () => {
 				<div onClick={() => setColor("red")} className={"light red" + (color === "red" ? " glow-red" : "")}></div>
 				<div onClick={() => setColor("yellow")} className={"light yellow" + (color === "yellow" ? " glow-yellow" : "")}></div>
 				<div onClick={() => setColor("green")} className={"light green" + (color === "green" ? " glow-green" : "")}></div>
+				{colors.includes("purple") && (
+					<div onClick={() => setColor("purple")} className={"light purple" + (color === "purple" ? " glow-purple" : "")}></div>
+				)}
 			</div>
 			<div>
-                <button className="btn btn-warning mt-3 ms-2">Add Purple</button>
+                <button className="btn btn-warning mt-3 ms-2" onClick={addPurple}>Add Purple</button>
             </div>
 		</div>
 	);
