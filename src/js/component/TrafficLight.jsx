40b232b8a95e675bbebcac39b10cd30ff3ea5e8e
Cesar Amaya Colella <cesar.rafael.gep@gmail.com>
2025-01-16 13:20:51 +0000

    the traffic light cycles using the state hook

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -1,7 +1,19 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 
 const TrafficLight = () => {
 	const [color, setColor] = useState("red")
+
+	const colors = ["red", "yellow", "green"];
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			const currentColor = colors.indexOf(color);
+			const nextColor = (currentColor + 1) % colors.length;
+			setColor(colors[nextColor])
+		}, 3000);
+
+		return () => clearInterval(interval);
+	}, [color])
 
 	return (
 		<div className="container">
@@ -11,6 +23,9 @@ const TrafficLight = () => {
 				<div onClick={() => setColor("yellow")} className={"light yellow" + (color === "yellow" ? " glow-yellow" : "")}></div>
 				<div onClick={() => setColor("green")} className={"light green" + (color === "green" ? " glow-green" : "")}></div>
 			</div>
+			<div>
+                <button className="btn btn-warning mt-3 ms-2">Add Purple</button>
+            </div>
 		</div>
 	);
 };
